d33334b086df3f3d9c6a79fe7f82052f8c059c38
Debanik <debanik96@gmail.com>
2017-02-24 19:55:12 +0530

    Added new Activities.

---
+++ app/src/main/java/com/project/classistant/AddSubjectStudentActivity.java
@@ -0,0 +1,13 @@
+package com.project.classistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddSubjectStudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_subject);
+    }
+}
---
+++ app/src/main/java/com/project/classistant/StudentMainActivity.java
@@ -0,0 +1,13 @@
+package com.project.classistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_main);
+    }
+}
