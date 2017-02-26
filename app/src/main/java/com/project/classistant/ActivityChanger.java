fcc320a6bfee6ea45c2fa5d489e50226ab2c3799
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2017-02-26 12:16:59 +0530

    Updating the Activity Changer Class.

--- app/src/main/java/com/project/classistant/ActivityChanger.java
+++ app/src/main/java/com/project/classistant/ActivityChanger.java
@@ -1,5 +1,6 @@
 package com.project.classistant;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -12,30 +13,58 @@ public class ActivityChanger {
 
     /**
      * NOTE: This is the method to change from one activity to another without any bundle data.
-     * @param currentActivity: The current activity from where you want to change.
+     * @param context: The current Activity context.
      * @param targetActivity: The activity where you want to go.
      */
-    public static void changeActivity(String currentActivity, String targetActivity){
+    public static void changeActivity(Context context,String targetActivity){
+        if(targetActivity.equalsIgnoreCase("addSubjectStudentActivity")){
+            intent=new Intent(context,AddSubjectStudentActivity.class);
+            context.startActivity(intent);
+        }
+        else if (targetActivity.equalsIgnoreCase("addSubjectTeacherActivity")){
+            intent=new Intent(context,AddSubjectTeacherActivity.class);
+            context.startActivity(intent);
+        }
+        else if(targetActivity.equalsIgnoreCase("RoutineActivity")){
+            intent=new Intent(context,RoutineActivity.class);
+            context.startActivity(intent);
+        }
+        else if (targetActivity.equalsIgnoreCase("StudentRegisterActivity")){
+            intent=new Intent(context,StudentRegisterActivity.class);
+            context.startActivity(intent);
+        }
+        else if (targetActivity.equalsIgnoreCase("TeacherRegisterActivity")){
+            intent=new Intent(context,TeacherRegisterActivity.class);
+            context.startActivity(intent);
+        }
+        else if(targetActivity.equalsIgnoreCase("StudentMainActivity")){
+            intent=new Intent(context,StudentMainActivity.class);
+            context.startActivity(intent);
+        }
+        else if(targetActivity.equalsIgnoreCase("TeacherMainActivity")){
+            intent=new Intent(context,TeacherMainActivity.class);
+            context.startActivity(intent);
+        }
     }
 
     /**
      * NOTE: This is the method to change from one activity to another without any bundle data.
-     * @param currentActivity: The current activity from where you want to change.
+     * @param context: The current activity context from where you want to change.
      * @param targetActivity: The activity where you want to go.
      * @param bundle: The data which is to be transfered to another Activity.
      */
-    public static void changeActivity(String currentActivity, String targetActivity, Bundle bundle){
+    public static void changeActivity(Context context, String targetActivity, Bundle bundle){
         intent.putExtras(bundle);
     }
 
     /**
      * NOTE: This is the method to change from one activity to another without any bundle data.
-     * @param currentActivity: The current activity from where you want to change.
+     * @param context: The current activity context from where you want to change.
      * @param targetActivity: The activity where you want to go.
      * @param key: The string key for the String data.
      * @param value: The string data which is to be transferred to the target activity.
      */
-    public static void changeActivity(String currentActivity, String targetActivity, String key, String value){
+    public static void changeActivity(Context context, String targetActivity, String key, String value){
         Bundle bundle=new Bundle();
         bundle.putString(key,value);
         intent.putExtras(bundle);
