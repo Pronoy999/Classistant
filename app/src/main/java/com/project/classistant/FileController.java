404fbecc670eaa1f871ecac410281d9b0d29e3e7
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2017-05-03 20:41:20 +0530

    Basic layout of Student Register and Student-Add Subjects.

--- app/src/main/java/com/project/classistant/FileController.java
+++ app/src/main/java/com/project/classistant/FileController.java
@@ -2,6 +2,10 @@ package com.project.classistant;
 
 import android.content.Context;
 
+import java.io.FileInputStream;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
 
 /**
  * Created by pronoymukherjee on 04/04/17.
@@ -12,5 +16,15 @@ public class FileController {
     public  FileController(Context context){
         this.context=context;
     }
+    public void create_loginFile(String account){
+        try{
+            OutputStreamWriter outputStreamWriter= new OutputStreamWriter(context.openFileOutput(Constant.LoginFile,Context.MODE_PRIVATE));
+            outputStreamWriter.write(account+" logged in,");
+            outputStreamWriter.close();
+        }
+        catch (Exception e){
+            Message.logMessages("ERROR ",e.toString());
+        }
     }
+}
 
--- app/src/main/java/com/project/classistant/MainActivity.java
+++ app/src/main/java/com/project/classistant/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     }
     private void goToTeacher(){
 
+
     }
     private void goToStudent(){
 
