529be06ac8373e01203d39a1b83d7c225db40cd1
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2017-07-23 10:41:20 +0530

    JXL Api added.

--- app/src/main/java/com/project/classistant/MainActivity.java
+++ app/src/main/java/com/project/classistant/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
